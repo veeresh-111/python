64869c33520304ccb804118aa87c48b9667d485b
veeresh-111 <68288620+veeresh-111@users.noreply.github.com>
2024-05-30 21:41:43 +0530

    Update all_the_file_handling.py

--- file_operations/all_the_file_handling.py
+++ file_operations/all_the_file_handling.py
@@ -43,7 +43,7 @@ if __name__ == '__main__':
 	filename = "example.txt"
 	new_filename = "new_example.txt"
 
-	create_file(filename)
+	write_file(filename)
 	read_file(filename)
 	append_file(filename, "This is some additional text.\n")
 	read_file(filename)
